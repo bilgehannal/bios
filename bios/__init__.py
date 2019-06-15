42b1166c6dd5d3a088f8224b80957b3f06c79beb
bilgehannal <bilgehanl.03@gmail.com>
2019-06-15 04:47:57 +0300

    package update,

--- bios/__init__.py
+++ bios/__init__.py
@@ -1,3 +1,3 @@
 from . import base
-from operations.standart_io import StandartIO
-from operations.csv_io import CsvIO
+from bios.operations.standart_io import StandartIO
+from bios.operations.csv_io import CsvIO
