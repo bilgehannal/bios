9f6f152243cd79505c260e95714180dc95e3d1cf
bilgehannal <bilgehanl.03@gmail.com>
2019-06-15 03:45:37 +0300

    license update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
   name = 'bios',         # How you named your package folder (MyLib)
   packages = ['bios'],   # Chose the same as "name"
   version = '0.0.1',      # Start with a small number and increase it with every change you make
-  license='GNU Affero General Public License v3.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'bios is a library which helps you to read and write data to determined type of files.',   # Give a short description about your library
   author = 'Bilgehan NAL',                   # Type in your name
   author_email = 'bilgehannal@gmail.com',      # Type in your E-Mail
@@ -18,7 +18,7 @@ setup(
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: GNU Affero General Public License v3.0',   # Again, pick a license
+    'License :: OSI Approved :: MIT',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
