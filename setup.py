34a2c7df35de23a06dcddb5d46e4962ac6cabebc
bilgehannal <bilgehanl.03@gmail.com>
2019-06-23 17:18:01 +0300

    aded description

--- setup.py
+++ setup.py
@@ -13,16 +13,16 @@ setup(
   version = '0.0.9',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'bios is a library which helps you to read and write data to determined type of files.',   # Give a short description about your library
-  long_description=long_description,
-  long_description_content_type='text/markdown'
+  long_description = long_description,
+  long_description_content_type = 'text/markdown',
   author = 'Bilgehan NAL',                   # Type in your name
   author_email = 'bilgehannal@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/bilgehannal/bios',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/bilgehannal/bios/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['io', 'read', 'write', 'file'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'PyYAML',
+          'oyaml',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
